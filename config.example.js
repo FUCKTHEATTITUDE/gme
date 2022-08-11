63672c22273a0bfb2220b2ed20ac2acdb8fcd07c
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2022-08-11 12:32:13 +0530

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,15 +1,15 @@
 var config = {
-  "token": "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
+  "token": "5458696791:AAHLcBi665AvhM9MA20YtqK64VzZ14a_U8E",
   "update_type": "long-polling", // or "webhook"
   "webhook": "https://www.example.com/<token>",
-  //"proxy": "http://127.0.0.1:8118",
-  //"bot_name": "example_bot", // check bot name,
-  "admin_id": "35197423",
+  "proxy": "http://139.162.168.208",
+  "bot_name": "Crocodile_gameebot", // check bot name,
+  "admin_id": "1930954213",
   "db": {
-    host: '127.0.0.1',
-    port: 27017,
-    //username: '',
-    //password: '',
+    host: '139.162.168.208',
+    port: 22,
+    username: 'root',
+    password: '20092001@Ss',
   }
 };
 
